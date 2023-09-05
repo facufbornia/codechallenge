491c750c65c62234d3bbe27cd3f84438881511de
facufbornia <141086748+facufbornia@users.noreply.github.com>
2023-09-04 22:23:04 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+document.getElementById("user-form").addEventListener("submit",function(e) {
+    const names = document.getElementById("input-name").value;
+    const secondName = document.getElementById("input-secName").value;
+    const date = document.getElementById("input-date").value;
+
+    let usuario = {
+        nombre: names,
+        apellido: secondName,
+        nacimiento: date
+    };
+
+    fetch("https://jsonplaceholder.typicode.com/users", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(usuario)})
+
+        .then(resp => resp.json())
+        .then(userForm => {
+    
+        console.log(userForm)
+       })
+       .catch(error => {
+        console.error('Error:', error);
+        });
+        e.preventDefault();
+    })
+   
